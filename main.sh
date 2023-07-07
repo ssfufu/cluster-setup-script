b25d0001c6dd440bc8a800d2a9af06181906e9df
Mathieu <48691168+ssfufu@users.noreply.github.com>
2023-07-07 11:42:01 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -31,6 +31,23 @@ nginx_setup() {
 
     # create a directory for this site if it doesn't exist
     touch /etc/nginx/sites-available/${CT_NAME}
+    # rm /etc/letsencrypt/options-ssl-nginx.conf
+    # touch /etc/letsencrypt/options-ssl-nginx.conf
+
+    # echo "# This file contains important security parameters. If you modify this file" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "# manually, Certbot will be unable to automatically provide future security" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "# updates. Instead, Certbot will print and log an error message with a path to" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "# the up-to-date file that you will need to refer to when manually updating" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "# this file." >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_session_cache shared:le_nginx_SSL:10m;" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_session_timeout 1440m;" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_session_tickets off;" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_protocols TLSv1.2 TLSv1.3;" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_prefer_server_ciphers off;" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "" >> /etc/letsencrypt/options-ssl-nginx.conf
+    # echo "ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";" >> /etc/letsencrypt/options-ssl-nginx.conf
 
     # substitute placeholders with variable values in the template and create a new config file
     sed -e "s#server_name#server_name ${SERVER_NAME};#g" \
@@ -43,8 +60,7 @@ nginx_setup() {
     
     systemctl stop nginx
 
-    certbot certonly --standalone -d ${SERVER_NAME} --email ${MAIL} --agree-tos --no-eff-email --noninteractive --force-renewal
-    
+    certbot certonly --nginx -d ${SERVER_NAME} --email ${MAIL} --agree-tos --no-eff-email --noninteractive --force-renewal
     systemctl start nginx
 
 }
@@ -67,12 +83,12 @@ vps_setup_single () {
     systemctl enable nginx && systemctl start nginx
     apt-get install lxc snapd -y
     sleep 2
-    apt-get install certbot python3-certbot-nginx -y
-    sleep 2
     snap install core
     sleep 2
     snap install lxd
     sleep 2
+    sudo snap install --classic certbot
+    ln -s /snap/bin/certbot /usr/bin/certbot
 
 
     adduser $SUDO_USER lxd
