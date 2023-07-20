497be26a64c14e4b3f891008df4a98d1e0f7f5a4
ssfufu <mathieu.medinas@gmail.com>
2023-07-20 11:01:47 +0200

    lxd boot service

--- main.sh
+++ main.sh
@@ -153,11 +153,12 @@ function lxc_lxd_setup () {
     snap install lxd > /dev/null
     sleep 2
 
-    systemctl enable snap.lxd.daemon.service
-    systemctl start snap.lxd.daemon.service
+    cp /root/cluster-setup-script/lxd/lxd_net.service /etc/systemd/system/lxd_net.service
+    systemctl daemon-reload
+    systemctl enable lxd_net.service
 
     adduser devops lxd
-    lxd init --preseed - < /root/cluster-setup-script/lxd_init.yaml
+    lxd init --preseed - < /root/cluster-setup-script/lxd/lxd_init.yaml
 
     lxc network create DMZ ipv4.address=10.128.151.1/24 ipv4.nat=true ipv4.dhcp=false
     lxc network create DMZ2 ipv4.address=10.128.152.1/24 ipv4.nat=true ipv4.dhcp=false
