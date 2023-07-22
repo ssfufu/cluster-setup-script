ab7c423b0c5f5e100203ad26e47d1a85b0db895d
ssfufu <mathieu.medinas@gmail.com>
2023-07-22 18:52:58 +0200

    apt-get update

--- main.sh
+++ main.sh
@@ -108,14 +108,14 @@ function docker_setup () {
     sleep 3
     apt-get install ca-certificates curl gnupg -y > /dev/null
 
-    install -m 0755 -d /etc/apt-get/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt-get/keyrings/docker.gpg
-    chmod a+r /etc/apt-get/keyrings/docker.gpg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
 
     echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt-get/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-        tee /etc/apt-get/sources.list.d/docker.list > /dev/null
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt-get update -y > /dev/null
     apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin debootstrap bridge-utils -y > /dev/null
