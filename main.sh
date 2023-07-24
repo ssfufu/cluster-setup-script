5a049e60033cc2c5b149183e09688107d7b819ae
ssfufu <mathieu.medinas@gmail.com>
2023-07-24 10:56:12 +0200

    a

--- main.sh
+++ main.sh
@@ -151,7 +151,11 @@ function wireguard_setup () {
     curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
     chmod +x wireguard-install.sh
     echo ""
+    echo ""
+    echo -e "\e[31--------------------------------------------------------------------------------------------\e[0m"
     echo -e "\e[31mWarning: Just press enter, to get the default config. This is important for the SSH config.\e[0m"
+    echo -e "\e[31--------------------------------------------------------------------------------------------\e[0m"
+    echo ""
     echo ""
     sleep 5
     ./wireguard-install.sh
@@ -385,14 +389,13 @@ function vps_setup_single () {
     chmod +x /root/updates.sh
     touch /etc/cron.d/auto_updates
     echo "0 2 * * * root /root/updates.sh" > /etc/cron.d/auto_updates
-
-
+    chmod 600 /etc/cron.d/auto_updates
 
     echo ""
     echo ""
     echo -e "-------------SETUP DONE-------------\n"
     echo "You now have a ready to use VPN (execute /root/wireguard_script/wireguard-install.sh for creating, removing clients.)"
-    echo "And also a cadvisor web pannel at cadvisor.$domain_user"
+    echo "And also a cadvisor web pannel at https://cadvisor.$domain_user"
 
     echo ""
 
