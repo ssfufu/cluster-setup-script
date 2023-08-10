775ba0c02d5ee82b0317a6868a2e5551933b6334
ssfufu <mathieu.medinas@gmail.com>
2023-08-10 11:33:32 +0200

    title

---
+++ scripts/cadvisor_version_updater.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cadvisor_latest_version=$(curl -s "https://gcr.io/v2/cadvisor/cadvisor/tags/list" | jq -r '.tags[]' | sort -V | tail -n 1)
+cadvisor_version_full="gcr.io/cadvisor/cadvisor:${cadvisor_latest_version}"
+sed -i "s|image:.*|image: ${cadvisor_version_full}|" /root/cluster-setup-script/cadvisor/docker-compose.yml
